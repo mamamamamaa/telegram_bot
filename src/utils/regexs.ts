4b4ee03b30ef891e544d85ebefe74db3d77a0876
mamamamamaa <maks.denishchuk@gmail.com>
2023-05-30 17:22:49 +0300

    fix up

--- src/utils/regexs.ts
+++ src/utils/regexs.ts
@@ -6,8 +6,5 @@ export const tiktokLinkRegex = /https?:\/\/vm\.tiktok\.com\/[a-zA-Z0-9]+\/?/;
 export const youtubeShortsLinkRegex =
   /^(?:https?:\/\/)?(?:www\.)?youtube\.com\/shorts\/[A-Za-z0-9_-]{11}$/;
 
-// export const youtubeMusicLinkRegex =
-//   /^(?:https?:\/\/)?(?:www\.)?music\.youtube\.com\/watch\?v=[A-Za-z0-9_-]{11}(?:&\S*)?$/;
-
 export const youtubeMusicLinkRegex =
-  /https?:\/\/(?:www\.)?music\.youtube\.com\/watch\?v=[A-Za-z0-9_-]{11}(?:&\S*)?$/;
+  /^(?:https?:\/\/)?(?:www\.)?music\.youtube\.com\/watch\?v=[A-Za-z0-9_-]{11}(?:&\S*)?$/;
